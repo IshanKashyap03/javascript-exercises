8d2c2bd09be15efd69674c401d3cc874d014fa13
ishankashyap <ikashyap@ualberta.ca>
2024-06-03 08:38:13 -0700

    reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let lastIndex = string.length - 1;
+    let result = "";
 
+    for(let i = lastIndex; i>=0; i--){
+        result+=string[i];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
