a3aa23f3d3ba12033e74ee8f25fe7e35b64900bb
ishankashyap <ikashyap@ualberta.ca>
2024-06-03 16:46:59 -0700

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+  return num1 - num2;
+};
+
+const sum = function(nums) {
+  let result = 0;
+  for(let i = 0; i<nums.length; i++){
+    result+=nums[i];
+  }
+  return result;
+};
+
+const multiply = function(nums) {
+  let result = 1;
+  for(let i = 0; i<nums.length; i++){
+    result*=nums[i];
+  }
+  return result;
+};
+
+const power = function(num1, num2) {
+  let result = 1;
+  for(let i = 0; i<num2; i++){
+    result*=num1;
+  }
+
+  return result;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(num) {
+  if(num == 0){
+    return 1;
+  }
 
-const sum = function() {
-	
-};
+  let result = 1;
+  for(let i = num; i>0; i--){
+    result*=i;
+  }
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  return result;
 };
 
 // Do not edit below this line
