647f95f7043e69aba10380fcd70f5b20434edd28
ishankashyap <ikashyap@ualberta.ca>
2024-06-04 09:38:43 -0700

    palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let regex = /^[A-Za-z0-9]*$/;
+    let left = 0;
+    let right = string.length - 1;
 
+    while(left < right){
+        while(!regex.test(string[left])){
+            left++;
+        }
+        while(!regex.test(string[right])){
+            right--;
+        }
+        if(string[left].toLowerCase() == string[right].toLowerCase()){
+            left++;
+            right--;
+        }else{
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
