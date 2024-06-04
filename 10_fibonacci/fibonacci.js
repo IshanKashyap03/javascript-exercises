806021e0b888fbdfdaecefc4fea16e55a699ea85
ishankashyap <ikashyap@ualberta.ca>
2024-06-04 09:55:22 -0700

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0){
+        return "OOPS";
+    }
+    let firstElement = 1;
+    let secondElement = 1;
+    let thirdElement = firstElement + secondElement;
+    let result = 0;
+
+    if(num == 1){
+        return firstElement;
+    }
+
+    if(num == 2){
+        return secondElement;
+    }
+
+    if(num == 3){
+        return thirdElement;
+    }
+
+    for(let i = 4; i<=num; i++){
+        result = thirdElement + secondElement;
+        secondElement = thirdElement;
+        thirdElement = result;
+    }
+
+    return result;
 
 };
 
